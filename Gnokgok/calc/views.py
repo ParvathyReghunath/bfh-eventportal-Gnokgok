1761fff8608a9bdd136b600b757fedb48dc50d39
Parvathy <parvathyreghunath.mec@gmail.com>
2021-05-18 20:32:44 +0530

    made new files

--- Gnokgok/calc/views.py
+++ Gnokgok/calc/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def add(request):
+    val1 = request.GET["num1"]
+    val2 = request.GET["num2"]
+    res = val1 + val2
+    return render(request, "result.html", {'result':res})
