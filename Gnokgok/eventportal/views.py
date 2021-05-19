627907714f55c2c712adaa37b611d7615d6df14a
Parvathy <parvathydeleep.mec@gmail.com>
2021-05-19 15:47:07 +0530

    Background image

--- Gnokgok/eventportal/views.py
+++ Gnokgok/eventportal/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Event
 
 def index(request):
-    return render(request, 'index.html')
+
+    event1 = Event()
+    return render(request, "index.html",{'event1': event1})
 
