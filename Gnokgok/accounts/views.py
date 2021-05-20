efd62b1acf293c6ed3d5bf1cc1baa0755afb5e24
Parvathy <parvathyreghunath.mec@gmail.com>
2021-05-20 10:38:20 +0530

    no change getting error

--- Gnokgok/accounts/views.py
+++ Gnokgok/accounts/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+
 def register(request):
+
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -11,11 +14,10 @@ def register(request):
         password2 = request.POST['password2']
         email = request.POST['email']
 
-        user = user.objects.create_user(username=username, password=password1 ,email=email ,first_name=first_name ,last_name=last_name)
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
         user.save();
         print('user created')
         return redirect('/')
-        return render(request,'register.html')
 
     else:
         return render(request, 'register.html')
